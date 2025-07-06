efe45d28bb674c1058c7d241b6cef594312130ea
Lakshya Harkhani <lakshyaharkhani@gmail.com>
2025-07-06 10:47:34 +0000

    use this image as the usetr image on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 <CardContent className="p-0">
                   <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-primary/20">
                     <Image
-                      src="https://placehold.co/400x400.png"
+                      src="/lakshya-portrait.jpg"
                       alt="Lakshya's Portrait"
                       width={400}
                       height={400}
