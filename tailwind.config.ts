8c9af596c274411628cc1489f80618ef4d259c19
Lakshya Harkhani <lakshyaharkhani@gmail.com>
2025-07-06 09:52:27 +0000

    now the timeline page is blank

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -109,6 +109,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
       },
     },
   },
